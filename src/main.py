23e0e392b6c589e9c9b8e73def3e9cf14f336605
arthur deryckere <arthurderyckere@outlook.com>
2021-01-27 14:18:46 +0100

    init local

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from djitellopy import Tello
+
+tello = Tello()
+
+tello.connect()
+tello.takeoff()
+
+tello.move_left(35)
+tello.move_right(35)
+tello.rotate_counter_clockwise(360)
+
+tello.land()
